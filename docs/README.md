fe0bb5a598647165dcda823c1375253b3e01ab2a
Alpha <alphahong@gmail.com>
2026-08-23 15:10:40 +0800

    Renamed chatbot and added greet and exit messages

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Chud User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Chud.java
@@ -0,0 +1,19 @@
+public class Chud {
+    public static void main(String[] args) {
+        String banner = "  ____ _               _ \n"
+                + " / ___| |__  _   _  __| |\n"
+                + "| |   | '_ \\| | | |/ _` |\n"
+                + "| |___| | | | |_| | (_| |\n"
+                + " \\____|_| |_|\\__,_|\\__,_|\n";
+        String horizontalLine = "____________________________________________________________";
+
+        System.out.println(horizontalLine);
+        System.out.print(banner);
+        System.out.println("Hello! I'm Chud.");
+        System.out.println("What can I do for you?");
+        System.out.println(horizontalLine);
+
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(horizontalLine);
+    }
+}
